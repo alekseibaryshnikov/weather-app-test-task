080f9703b3743d4cb366025e72be0bb41e7c9e28
Aleksei Baryshnikov <a250188@yandex.ru>
2020-08-21 01:02:30 +0300

    Added business logic for the cards.

---
+++ src/components/Screens/Weather/WeatherCards/WeatherCard/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+
+export default function WeatherCard(props) {
+    return <Card>
+            <CardContent>
+                asd
+            </CardContent>
+        </Card>;
+}
--- src/components/Screens/Weather/WeatherCards/index.jsx
+++ src/components/Screens/Weather/WeatherCards/index.jsx
@@ -1,22 +1,73 @@
 import React from 'react';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
 import Grid from '@material-ui/core/Grid';
 import Container from '../../../Common/Container';
+import WeatherCard from './WeatherCard';
+import { useSelector } from 'react-redux';
+import { selectWeatherData } from '../../../../redux/features/weatherReducer';
+
 
 export default function () {
+    const weatherData = useSelector(selectWeatherData);
+    const cards = prepareCards(weatherData);
+
     return <Container>
         <Grid container justify="center" spacing={2}>
             {[0, 1, 2].map((value) => (
-                <Grid key={value} item>
-                    <Card>
-                        <CardContent>
-                            asd
-                            </CardContent>
-                    </Card>
+                <Grid item key={value}>
+                    <WeatherCard />
                 </Grid>
             ))}
         </Grid>
     </Container>;
+}
 
+function prepareCards(weatherData) {
+    const cards = [];
+    const today = new Date();
+
+    for (let i = 0; i < 5; i++) {
+        if (i > 0) {
+            today.setDate(today.getDate() + 1);
+        }
+
+        const tmp = summarize(today, weatherData);
+        cards.push(tmp)
+    }
+
+    return cards;
+}
+
+function summarize(referenceDate, weatherData) {
+    const summarizedDay = weatherData.properties.timeseries.filter(item => {
+        const itemDate = new Date(item.time);
+        const equalDates = itemDate.getFullYear() === referenceDate.getFullYear() &&
+            itemDate.getMonth() === referenceDate.getMonth() &&
+            itemDate.getDate() === referenceDate.getDate();
+        const dayTime = itemDate.getHours() > 10 && itemDate.getHours() < 20;
+
+        return equalDates && dayTime;
+    }).reduce((acc, curr, idx, src) => {
+        const { data: { instant: { details: { air_temperature, air_pressure_at_sea_level } } } } = curr;
+
+        if (idx === 0) {
+            const middle = src[Math.floor(src.length / 2)];
+            const { data: { next_6_hours: { summary: { symbol_code } } } } = middle;
+            acc.weatherType = symbol_code;
+            acc.date = referenceDate;
+        }
+        
+        acc.temperature = air_temperature + acc.temperature;
+        acc.pressure = air_pressure_at_sea_level + acc.pressure;
+        acc.counter = acc.counter + 1;
+        acc.date = acc.date ? acc.date : referenceDate;
+
+        return acc;
+    }, { temperature: 0, pressure: 0, counter: 0, date: null, weatherType: null });
+
+    return {
+        temperature: Math.round(summarizedDay.temperature / summarizedDay.counter),
+        pressure: Math.round(summarizedDay.pressure / summarizedDay.counter),
+        weatherType: summarizedDay.weatherType,
+        date: summarizedDay.date
+    };
 }
