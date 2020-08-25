6ec2785d47b65a17bca059ebfe1441fca4aa2183
Aleksei Baryshnikov <a250188@yandex.ru>
2020-08-25 21:37:59 +0300

    Updates

---
+++ precache-manifest.e1dbf5701e307e6c166dcc5d5f1aa8d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "401d7ad5e090c8aa2612301e2defc7b5",
+    "url": "/weather-app-test/index.html"
+  },
+  {
+    "revision": "08f5373f5c674a27be35",
+    "url": "/weather-app-test/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b65d4be07d5c39986020",
+    "url": "/weather-app-test/static/js/2.b15aa2c1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/weather-app-test/static/js/2.b15aa2c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08f5373f5c674a27be35",
+    "url": "/weather-app-test/static/js/main.12d3daae.chunk.js"
+  },
+  {
+    "revision": "ab18eb1426edbb6928ef",
+    "url": "/weather-app-test/static/js/runtime-main.9bb807dc.js"
+  }
+]);
