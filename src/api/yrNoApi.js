cf1375da032b8c6f1fd3edd17e51ca78815b9157
alekseibaryshnikov <a250188@yandex.ru>
2020-08-25 18:54:16 +0300

    Added default location for Munich

--- src/api/yrNoApi.js
+++ src/api/yrNoApi.js
@@ -1,15 +1,20 @@
 export async function getWeather() {
+    let coords = { latitude: '', longitude: '' };
+
     try {
-        const {coords: {latitude, longitude}} = await getCurrentPostion();
-        const response = await fetch(`https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${latitude}&lon=${longitude}`);
-        
-        if (response.ok) {
-            return response.json();
-        } else {
-            throw Error(`Response status is ${response.status}`);
-        }
+        const { coords: { latitude, longitude } } = await getCurrentPostion();
+        coords = { latitude, longitude };
     } catch (error) {
-        console.error(`Error when was fetching weather data.. ${error}`);
+        console.warn('Error with geolocation API.. Set default Munich weather..', error);
+        coords = { longitude: '48.1351', latitude: '11.5820' }
+    }
+
+    const response = await fetch(`https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${coords.latitude}&lon=${coords.longitude}`);
+
+    if (response.ok) {
+        return response.json();
+    } else {
+        throw Error(`Response status is ${response.status}`);
     }
 }
 
